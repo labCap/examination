8fbf76fe256efdbbb1039ef104927bef955da590
banlazthosow@gmail.com <75088990+labCap@users.noreply.github.com>
2022-07-30 17:04:36 +0300

    update v1

---
+++ gulp/config/path.js
@@ -0,0 +1,36 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        js: `${srcFolder}/js/app.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        // sass: `${srcFolder}/sass/style.sass`,
+        scss: `${srcFolder}/scss/*.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        // sass: `${srcFolder}/sass/**/*.sass`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg}`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+};
